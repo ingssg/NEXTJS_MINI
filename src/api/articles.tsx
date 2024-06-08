c84fb18ebe2ec386956e12687491072cbfb06633
Inseok Kim <js2121good@gmail.com>
2024-06-09 00:31:49 +0900

    feat: post article

--- src/api/articles.tsx
+++ src/api/articles.tsx
@@ -1,12 +1,23 @@
-import useFetchData from "./hooks";
+import { useFetchData, usePostData } from "./hooks";
 
 export const getArticles = async () => {
-  const {data, loading, error} = useFetchData("/board");
+  const { data, loading, error } = useFetchData("/board");
   if (loading) {
     return <div>로딩 중입니다.</div>;
   }
   if (error) {
-    return <div>에러가 발생했습니다.</div>
+    return <div>에러가 발생했습니다.</div>;
   }
   return data;
-}
+};
+
+export const postArticle = async (data: object) => {
+  const { loading, error } = usePostData("/board", data);
+  if (loading) {
+    return <div>로딩 중입니다.</div>;
+  }
+  if (error) {
+    return <div>에러가 발생했습니다.</div>;
+  }
+  return <div>게시글이 등록되었습니다.</div>;
+};
--- src/api/hooks.tsx
+++ src/api/hooks.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios, { AxiosError } from 'axios';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
 
 type FetchDataResponse<T> = {
   data: T | null;
@@ -7,7 +9,7 @@ type FetchDataResponse<T> = {
   error: AxiosError | null;
 };
 
-const useFetchData = <T,>(url: string): FetchDataResponse<T> => {
+export const useFetchData = <T,>(url: string): FetchDataResponse<T> => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<AxiosError | null>(null);
@@ -29,4 +31,20 @@ const useFetchData = <T,>(url: string): FetchDataResponse<T> => {
   return { data, loading, error };
 };
 
-export default useFetchData;
+export const usePostData = <T,>(url: string, data: object) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<AxiosError | null>(null);
+
+  const postData = async () => {
+    try {
+      await axios.post<T>(url, data);
+    } catch (e) {
+      setError(e as AxiosError);
+    } finally {
+      setLoading(false);
+    }
+  };
+  postData();
+
+  return { loading, error };
+};
