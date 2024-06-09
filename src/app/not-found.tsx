00a4031c3f0152c35de1a4a0f0261e37bedfcae2
Inseok Kim <js2121good@gmail.com>
2024-06-09 17:21:57 +0900

    delete: delete env, add priority to 404 img

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center gap-7 h-screen mb-[-10vh]">
-      <Image src="/404error.png" alt="404 error" width={400} height={300} />
+      <Image src="/404error.png" alt="404 error" width={400} height={400} priority />
       <h1 className="text-2xl">해당 페이지를 찾을 수 없습니다.</h1>
     </div>
   );
